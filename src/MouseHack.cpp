3ec1e563cd27652d36435e0a814d41214058a416
uriu-make <yoshinori.3775@outlook.jp>
2021-08-29 19:10:51 +0900

    first commit

---
+++ src/MouseHack.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+  return 0;
+}
